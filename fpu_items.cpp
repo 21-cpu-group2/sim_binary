d816e4e9e1a3e21d3b3864833ef424e47f987a0c
Daichi-Hayakawa <hayakawadaichi001@g.ecc.u-tokyo.ac.jp>
2021-12-21 18:45:45 +0900

    velilog模倣に必要な基本的な操作を実装(concat add sub mul)

---
+++ fpu_items.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <iomanip>
+#include "fpu_items.hpp"
+
+#define vd velilog_data
+using namespace std;
+
+int main(void){
+    velilog_data v0 = {0x00000005, 4};
+    velilog_data v1 = {0x00000002, 4};
+    velilog_data v2 = {0x000000FD, 8};
+    vd v3 = {0x0000FE33, 16};
+    v3 = slice(v3, 11, 4);
+    cout << hex << concat2(v0, v1).data << endl;
+    cout << concat3(v0, v1, v2).data << endl;
+    cout << concat4(v0, v1, v2, v3).data << endl;
+    cout << add(v0, v1).data << endl;
+    cout << sub(v0, v1).data << endl;
+    cout << mul(v0, v1).data << endl;
+    return 0;
+}
--- fpu_items.hpp
+++ fpu_items.hpp
@@ -1,8 +1,108 @@
 #include <iostream>
 #include <string>
+#include <stdlib.h>
+
+using namespace std;
 
 typedef struct {
-    uint32_t sgn : 1;
-    uint32_t exp : 8;
-    uint32_t fra : 23;
+    uint32_t data;
+    uint32_t bit_num;
+} velilog_data;
+
+typedef struct {
+    velilog_data sgn;
+    velilog_data exp;
+    velilog_data fra;
 } my_float;
+
+inline uint32_t bit_mask(uint32_t n){
+    if (n == 0) return 0x00000000;
+    return 0xFFFFFFFF >> (32-n);
+}
+
+inline velilog_data concat2(velilog_data dat1, velilog_data dat2){
+    uint32_t ret_data = 0;
+    uint32_t ret_bit_num = 0;
+    ret_data |= (dat1.data) & bit_mask(dat1.bit_num);
+    ret_bit_num += dat1.bit_num;
+    ret_data <<= dat2.bit_num;
+    ret_data |= (dat2.data) & bit_mask(dat2.bit_num);
+    ret_bit_num += dat2.bit_num;
+    velilog_data ret = {ret_data, ret_bit_num};
+    return ret;
+}
+
+inline velilog_data concat3(velilog_data dat1, velilog_data dat2, velilog_data dat3){
+    uint32_t ret_data = 0;
+    uint32_t ret_bit_num = 0;
+    ret_data |= (dat1.data) & bit_mask(dat1.bit_num);
+    ret_bit_num += dat1.bit_num;
+    ret_data <<= dat2.bit_num;
+    ret_data |= (dat2.data) & bit_mask(dat2.bit_num);
+    ret_bit_num += dat2.bit_num;
+    ret_data <<= dat3.bit_num;
+    ret_data |= (dat3.data) & bit_mask(dat3.bit_num);
+    ret_bit_num += dat3.bit_num;
+    velilog_data ret = {ret_data, ret_bit_num};
+    return ret;
+}
+
+inline velilog_data concat4(velilog_data dat1, velilog_data dat2, velilog_data dat3, velilog_data dat4){
+    uint32_t ret_data = 0;
+    uint32_t ret_bit_num = 0;
+    ret_data |= (dat1.data) & bit_mask(dat1.bit_num);
+    ret_bit_num += dat1.bit_num;
+    ret_data <<= dat2.bit_num;
+    ret_data |= (dat2.data) & bit_mask(dat2.bit_num);
+    ret_bit_num += dat2.bit_num;
+    ret_data <<= dat3.bit_num;
+    ret_data |= (dat3.data) & bit_mask(dat3.bit_num);
+    ret_bit_num += dat3.bit_num;
+    ret_data <<= dat4.bit_num;
+    ret_data |= (dat4.data) & bit_mask(dat4.bit_num);
+    ret_bit_num += dat4.bit_num;
+    velilog_data ret = {ret_data, ret_bit_num};
+    return ret;
+}
+
+inline velilog_data slice(velilog_data dat, uint32_t to, uint32_t from){
+    uint32_t ret_data = 0;
+    uint32_t ret_bit_num = to - from + 1;
+    ret_data = (dat.data >> from) & bit_mask(to - from + 1);
+    velilog_data ret = {ret_data, ret_bit_num};
+    return ret;
+}
+
+// //adder subtracter multiplier shift
+inline velilog_data add(velilog_data r1, velilog_data r2){
+    if (r1.bit_num != r2.bit_num) {
+        cout << "error input length differ" << endl;
+        exit(1);
+    }
+    uint32_t ret_data = 0;
+    ret_data = (r1.data + r2.data) & bit_mask(r1.bit_num);
+    velilog_data ret = {ret_data, r1.bit_num};
+    return ret;
+}
+
+inline velilog_data sub(velilog_data r1, velilog_data r2){
+    if (r1.bit_num != r2.bit_num) {
+        cout << "error input length differ" << endl;
+        exit(1);
+    }
+    uint32_t ret_data = 0;
+    ret_data = (r1.data - r2.data) & bit_mask(r1.bit_num);
+    velilog_data ret = {ret_data, r1.bit_num};
+    return ret;
+}
+
+inline velilog_data mul(velilog_data r1, velilog_data r2){
+    if (r1.bit_num != r2.bit_num) {
+        cout << "error input length differ" << endl;
+        exit(1);
+    }
+    uint32_t ret_data = 0;
+    ret_data = (r1.data * r2.data) & bit_mask(r1.bit_num);
+    velilog_data ret = {ret_data, r1.bit_num};
+    return ret;
+}

b07a7312ca1dfb5ced4822d3697f94f3fee9a4ff
Daichi-Hayakawa <73731841+Daichi-Hayakawa@users.noreply.github.com>
2021-12-24 12:19:38 +0900

    Update fless.hpp

---
+++ fpu/fless.hpp
@@ -0,0 +1,32 @@
+#ifndef _FLESS
+#define _FLESS
+#include "fpu_items.hpp"
+using namespace std;
+
+vd fless(vd op1, vd op2){
+    vd sig1 = slice(op1, 31, 31);
+    vd sig2 = slice(op2, 31, 31);
+    vd exp1 = slice(op1, 30, 23);
+    vd exp2 = slice(op2, 30, 23);
+    if (equal(sig1, sig2) && equal(exp1, exp2)) {
+        if (lt(op1, op2)){
+            return make_vd(1,1);
+        }
+        else {
+            return make_vd(0,1);
+        }
+    }
+    else if (equal(sig1, sig2)) {
+        if (lt(exp1, exp2)) {
+            return make_vd(1,1);
+        }
+        else{
+            return make_vd(0,1);
+        }
+    }
+    else{
+        return sig1;
+    }
+}
+
+#endif

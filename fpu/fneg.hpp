d8f9ad17e8163704c702b5d9508435371de68efa
Laplace-LT <makoto.akiyama.0228@gmail.com>
2022-01-03 16:42:25 +0900

    Update fneg.hpp

--- fpu/fneg.hpp
+++ fpu/fneg.hpp
@@ -5,7 +5,7 @@ using namespace std;
 
 inline vd fneg(vd op){
     vd result;
-    assign(&result, vd_not(slice(op, 31, 31)), 31, 31);
+    assign(&result, vd_and(vd_not(slice(op, 31, 31)), vd_or_red(slice(op, 30, 23))), 31, 31);
     assign(&result, slice(op, 30, 0), 30, 0);
     return result;
 }

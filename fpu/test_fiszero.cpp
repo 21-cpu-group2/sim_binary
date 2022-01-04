82e2975f8439d598dcd8a51e62ef9d55927f532d
Daichi-Hayakawa <hayakawadaichi001@g.ecc.u-tokyo.ac.jp>
2022-01-04 18:15:32 +0900

    fiszero完了

--- fpu/test_fiszero.cpp
+++ fpu/test_fiszero.cpp
@@ -4,6 +4,30 @@
 #include <iomanip>
 #include "fiszero.hpp"
 #define DEBUG 1
+#define CHECK 1
+
+int check(){
+    bool flg = true;
+    for (uint32_t ite=0; ite<0xFFFFFFFF; ite++){
+        if (isNumber(ite)){
+            union fi input, output, correct;
+            input.i = ite;
+            vd v = {ite, 32};
+            vd result = fiszero(v);
+            output.i = result.data;
+            if (((input.f == 0) ^ (result.data))){
+                flg = false;
+            }
+        }
+    }
+    if (flg){
+        cout << "test passed" << endl;
+    }
+    else {
+        cout << "test failed" << endl;
+    }
+    return 0;
+}
 
 int test_simulator(){
     ifstream in("../../fpu/fiszero/sample_fiszero.txt");
@@ -27,36 +51,16 @@ int test_simulator(){
 }
 
 int main(){
+    if (CHECK){
+        cout << "check_simulator" << endl;
+        check();
+        return 0;
+    }
     cout << hex ;
     if (DEBUG) {
         cout << "testing_simulator" << endl;
         test_simulator();
         return 0;
-    }
-    union fi v_fi1, v_fi2;
-    int limit_print = 10;
-    int print_num = 0;
-    for (uint32_t ite=0x00000000; ite<0xFFFFFFFF; ite++){
-        union fi v_fi1, v_fi2;
-        v_fi1.i = ite;
-        if (!isNumber(v_fi1.f)) continue;
-        vd v = {v_fi1.i, 32};
-        vd result;
-        result = fiszero(v);
-        v_fi2.i = result.data;
-        if(((result.data == 1) ^ (v_fi1.f == 0.))) {
-            cout << "error" << endl;
-            bit_print(v_fi1.i);
-            bit_print(v_fi2.i);
-            cout << v_fi1.f << endl;
-            print_num++;
-        }
-        if (print_num > limit_print){
-            break;
-        }
-        if ((ite & 0x0FFFFFFF) == 0){
-            cout << "10%" << endl;
-        }
     }
     return 0;
 }

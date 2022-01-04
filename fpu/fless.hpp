92cc3fce138f8d66efa7e1932df77aee68be00ed
Daichi-Hayakawa <hayakawadaichi001@g.ecc.u-tokyo.ac.jp>
2022-01-04 22:05:37 +0900

    flessを修正

--- fpu/fless.hpp
+++ fpu/fless.hpp
@@ -4,24 +4,29 @@
 using namespace std;
 
 inline vd fless(vd op1, vd op2){
-    vd sig1 = slice(op1, 31, 31);
-    vd sig2 = slice(op2, 31, 31);
-    vd exp1 = slice(op1, 30, 23);
-    vd exp2 = slice(op2, 30, 23);
-    if (equal(sig1, sig2) && equal(exp1, exp2)) {
-        if (lt(op1, op2)){
-            return make_vd(1,1);
+    vd sig1 = {0, 1};
+    assign(&sig1, slice(op1, 31, 31), -1, -1);
+    vd sig2 = {0, 1};
+    assign(&sig2, slice(op2, 31, 31), -1, -1);
+    vd exp1 = {0, 8};
+    assign(&exp1, slice(op1, 30, 23), -1, -1);
+    vd exp2 = {0, 8};
+    assign(&exp2, slice(op2, 30, 23), -1, -1);
+
+    if ((sig1.data == sig2.data) && (exp1.data == exp2.data)) {
+        if ((slice(op1, 22, 0).data < slice(op2, 22, 0).data) ^ (sig1.data == 1)){
+            return constant(1,1);
         }
         else {
-            return make_vd(0,1);
+            return constant(0,1);
         }
     }
-    else if (equal(sig1, sig2)) {
-        if (lt(exp1, exp2)) {
-            return make_vd(1,1);
+    else if (sig1.data ==  sig2.data) {
+        if ((exp1.data < exp2.data) ^ (sig1.data == 1)) {
+            return constant(1,1);
         }
         else{
-            return make_vd(0,1);
+            return constant(0,1);
         }
     }
     else{
